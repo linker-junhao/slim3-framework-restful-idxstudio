d400a464b5ed23e0c05946dba04f2c79e8bed644
linkerGitHub <linker-junhao@outlook.com>
2018-12-24 15:57:55 +0800

    完善表单验证类，并增加本地化错误提示，还剩余正则、文件、字段间比较

--- src/idxLib/util/FormValidation/Validation.php
+++ src/idxLib/util/FormValidation/Validation.php
@@ -10,6 +10,7 @@
 namespace IdxLib\util\FormValidation;
 
 
+use IdxLib\util\FormValidation\Local\ErrMsgCN;
 use Interop\Container\ContainerInterface;
 
 class Validation
@@ -19,136 +20,282 @@ class Validation
     private $postParams;
     private $files;
 
+    private $resultInfo;
+
     public function __construct(ContainerInterface $ci)
     {
         $this->ci = $ci;
         $this->getParams = count($this->ci->request->getQueryParams()) == 0 ? null : $this->ci->request->getQueryParams();
         $this->postParams = count($this->ci->request->getParsedBody()) == 0 ? null : $this->ci->request->getParsedBody();
         $this->files = count($this->ci->request->getUploadedFiles()) == 0 ? null : $this->ci->request->getUploadedFiles();
+        $this->resultInfo = array(
+            'get' => array(),
+            'post' => array()
+        );
     }
 
+    /**
+     * set get query parameters valid regulation
+     * @param array $regulation
+     * @return $this
+     */
     public function setQueryParamRegulation(array $regulation)
     {
         foreach ($regulation as $fieldName => $fieldRegulation) {
-
+            $this->resultInfo['get'][$fieldName] = array();
+            $this->distributeRegulation($this->getParams[$fieldName], $fieldRegulation, $this->resultInfo['get'][$fieldName]);
         }
+        return $this;
     }
 
+    /**
+     * set post form valid regulation
+     * @param array $regulation
+     * @return $this
+     */
     public function setPostParamRegulation(array $regulation)
     {
-
+        foreach ($regulation as $fieldName => $fieldRegulation) {
+            $this->resultInfo['post'][$fieldName] = array();
+            $this->distributeRegulation($this->postParams[$fieldName], $fieldRegulation, $this->resultInfo['post'][$fieldName]);
+        }
+        return $this;
     }
 
-    private function distributeRegulation($target, $regulation)
+
+    /**
+     * set valid result info to view data
+     * @return array
+     */
+    public function validDone()
+    {
+        $this->ci->view->offsetSet('validErr', $this->resultInfo);
+        return $this->resultInfo;
+    }
+
+    /**
+     * get the valid result of validation
+     * @return array
+     */
+    public function getResult()
+    {
+        return $this->resultInfo;
+    }
+
+    /**
+     * distribute the valid to different valid function
+     * and set the error information
+     * @param $target
+     * @param $regulation
+     * @param $resultArray
+     * @return bool
+     */
+    private function distributeRegulation($target, $regulation, &$resultArray)
     {
         $regArray = explode('|', $regulation);
+        $testResult = true;
 
-
+        $resultInfo = '';
         //required，表示该项必填
         if (in_array('required', $regArray)) {
-            $this->required($target);
+            if (!($test = $this->required($target))) {
+                $resultInfo .= ErrMsgCN::$ErrMsgCN['required'];
+            }
+            $testResult = $testResult && $test;
         }
 
         //no_space，无空格
         if (in_array('no_space', $regArray)) {
-            $this->noSpace($target);
+            if (!($test = $this->noWhiteSpace($target))) {
+                $resultInfo .= ErrMsgCN::$ErrMsgCN['noWhiteSpace'];
+            }
+            $testResult = $testResult && $test;
         }
 
         //email，邮件格式
         if (in_array('email', $regArray)) {
-            $this->eMail($target);
+            if (!($test = $this->eMail($target))) {
+                $resultInfo .= ErrMsgCN::$ErrMsgCN['eMail'];
+            }
+            $testResult = $testResult && $test;
         }
 
         //numeric，都是数字
         if (preg_grep('/^(numeric:)([0-9]{0,})~([0-9]{0,})$/', $regArray)) {
-            $this->allNumeric($target);
+            if (!($test = $this->allNumeric($target))) {
+                $resultInfo .= ErrMsgCN::$ErrMsgCN['allNumeric'];
+            }
+            $testResult = $testResult && $test;
         }
 
         //alpha，全部都是英文字母
         if (in_array('alpha', $regArray)) {
-            $this->allAlpha($target);
+            if (!($test = $this->allAlpha($target))) {
+                $resultInfo .= ErrMsgCN::$ErrMsgCN['allAlpha'];
+            }
+            $testResult = $testResult && $test;
         }
 
         //alpha_dash，可以是英文、数字、下划线(_)和短横线(-)
         if (in_array('alpha_dash', $regArray)) {
-            $this->alphaDash($target);
+            if (!($test = $this->alphaDash($target))) {
+                $resultInfo .= ErrMsgCN::$ErrMsgCN['alphaDash'];
+            }
+            $testResult = $testResult && $test;
         }
 
         //alpha_num，必须完全是字母、数字。
         if (in_array('alpha_num', $regArray)) {
-            $this->alphaNum($target);
+            if (!($test = $this->alphaNum($target))) {
+                $resultInfo .= ErrMsgCN::$ErrMsgCN['alphaNum'];
+            }
+            $testResult = $testResult && $test;
         }
 
         //phone，电话号码
         if (in_array('phone', $regArray)) {
-            $this->phone($target);
+            if (!($test = $this->phone($target))) {
+                $resultInfo .= ErrMsgCN::$ErrMsgCN['phone'];
+            }
+            $testResult = $testResult && $test;
         }
 
         //date，日期
-        if (preg_grep('/^(date:)([0-9]{4}-[0-9]{2}-[0-9]{2})~([0-9]{4}-[0-9]{2}-[0-9]{2})$/', $regArray)) {
-
+        if (count($pregResult = preg_grep('/^(date:)([0-9]{4}-(0[1-9]|1[0-2])-((0[1-9])|((1|2)[0-9])|30|31))~([0-9]{4}-(0[1-9]|1[0-2])-((0[1-9])|((1|2)[0-9])|30|31))$/', $regArray))) {
+            if (!($test = $this->date($target, $pregResult[0]))) {
+                $resultInfo .= ErrMsgCN::$ErrMsgCN['date'];
+            }
+            $testResult = $testResult && $test;
         }
 
         //datetime，日期时间
-        if (preg_grep('/^(date:)([0-9]{4}-[0-9]{2}-[0-9]{2} [0-2][0-9]:[0-5][0-9]:[0-5][0-9])~([0-9]{4}-[0-9]{2}-[0-9]{2} [0-2][0-9]:[0-5][0-9]:[0-5][0-9])$/', $regArray)) {
-
+        if (count($pregResult = preg_grep('/^(datetime:)(([0-9]{4}-(0[1-9]|1[0-2])-((0[1-9])|((1|2)[0-9])|30|31)) ((0|1)[0-9]|2[0-4]):([0-5][0-9]):([0-5][0-9]))~(([0-9]{4}-(0[1-9]|1[0-2])-((0[1-9])|((1|2)[0-9])|30|31)) ((0|1)[0-9]|2[0-4]):([0-5][0-9]):([0-5][0-9]))$/', $regArray))) {
+            $testResult = $testResult && $this->dateTime($target, $pregResult[0]);
+            if (!($test = $this->dateTime($target, $pregResult[0]))) {
+                $resultInfo .= ErrMsgCN::$ErrMsgCN['dateTime'];
+            }
+            $testResult = $testResult && $test;
         }
 
+        if (preg_grep('/^(reg:)(.|\n|\r)+$/', $regArray)) {
+            if (!($test = $this->regExp($target, 'ss'))) {
+                $resultInfo .= ErrMsgCN::$ErrMsgCN['regExp'];
+            }
+            $testResult = $testResult && $test;
+        }
+
+        $resultArray['textInfo'] = $resultInfo;
+        $resultArray['status'] = $testResult;
+        return $testResult;
     }
 
-    private function required($target)
+    /**
+     * @param $target
+     * @return bool
+     */
+    public function required($target)
     {
-        return count_chars($target);
+        return strlen($target) == 0 ? false : true;
     }
 
-    private function noSpace($target)
+    /**
+     * @param $target
+     * @return bool
+     */
+    public function noWhiteSpace($target)
     {
-
+        return !preg_match('/\s/', $target);
     }
 
-    private function eMail($target)
+    /**
+     * @param $target
+     * @return false|int
+     */
+    public function eMail($target)
     {
-
+        return preg_match('/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/', $target);
     }
 
-    private function allNumeric($target)
+    /**
+     * @param $target
+     * @return false|int
+     */
+    public function allNumeric($target)
     {
-
+        return preg_match('/^[0-9]+$/', $target);
     }
 
-    private function allAlpha($target)
+    /**
+     * @param $target
+     * @return false|int
+     */
+    public function allAlpha($target)
     {
-
+        return preg_match('/^[a-zA-Z]+$/', $target);
     }
 
-    private function alphaDash($target)
+    /**
+     * alpha_dash，可以是英文、数字、下划线(_)和短横线(-)
+     */
+    public function alphaDash($target)
     {
-
+        return preg_match('/^[a-zA-Z0-9|_|-]+$/', $target);
     }
 
-    private function alphaNum($target)
+    /**
+     * @param $target
+     * @return false|int
+     */
+    public function alphaNum($target)
     {
-
+        return preg_match('/^[a-zA-Z0-9]$/', $target);
     }
 
-    private function phone($target)
+    /**
+     * @param $target
+     * @return false|int
+     */
+    public function phone($target)
     {
-
+        return preg_match('/^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/', $target);
     }
 
-    private function date($target)
+    /**
+     * @param $target
+     * @param $regulation
+     * @return bool
+     */
+    public function date($target, $regulation)
     {
-
+        preg_match('/^(date:)([0-9]{4}-(0[1-9]|1[0-2])-((0[1-9])|((1|2)[0-9])|30|31))~([0-9]{4}-(0[1-9]|1[0-2])-((0[1-9])|((1|2)[0-9])|30|31))$/', $regulation, $regResult);
+        $minData = strtotime($regResult[2]);
+        $maxDate = strtotime($regResult[8]);
+        $targetDate = strtotime($target);
+        return ($targetDate > $minData) && ($targetDate < $maxDate) && $targetDate;
     }
 
-    private function dateTime($target)
+    /**
+     * @param $target
+     * @param $regulation
+     * @return bool
+     */
+    public function dateTime($target, $regulation)
     {
-
+        preg_match('/^(datetime:)(([0-9]{4}-(0[1-9]|1[0-2])-((0[1-9])|((1|2)[0-9])|30|31)) ((0|1)[0-9]|2[0-4]):([0-5][0-9]):([0-5][0-9]))~(([0-9]{4}-(0[1-9]|1[0-2])-((0[1-9])|((1|2)[0-9])|30|31)) ((0|1)[0-9]|2[0-4]):([0-5][0-9]):([0-5][0-9]))$/', $regulation, $regResult);
+        $minData = strtotime($regResult[1]);
+        $maxDate = strtotime($regResult[12]);
+        $targetDate = strtotime($target);
+        return ($targetDate > $minData) && ($targetDate < $maxDate) && $targetDate;
     }
 
-    private function regExp($target)
+    /**
+     * @param $target
+     * @param $regulation
+     * @return int
+     */
+    public function regExp($target, $regulation)
     {
-
+        return 1;
     }
 
     private function fileSize($target)
@@ -171,4 +318,8 @@ class Validation
 
     }
 
+    private function errorInfoConcat()
+    {
+
+    }
 }
