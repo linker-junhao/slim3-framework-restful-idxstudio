8de1945f6a03c209ce4f94abefb2763d642228b7
linkerGitHub <linker-junhao@outlook.com>
2018-12-29 16:06:25 +0800

    1.在slimRestful下增加HttpResponseBody类

---
+++ src/idxLib/Middleware/SlimRestful/Standard/HttpResponseBody.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User : Linker
+ * Email: linker-junhao@outlook.com
+ * Date : 2018/12/29
+ * Time : 15:56
+ */
+
+namespace IdxLib\Middleware\SlimRestful\Standard;
+
+
+class HttpResponseBody
+{
+
+}
