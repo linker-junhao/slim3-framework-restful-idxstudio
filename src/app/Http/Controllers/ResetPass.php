36934e70d27804b780d2227b0067fc026e6e1c55
haolang <haolangtaiye@gmail.com>
2018-12-29 15:34:25 +0800

    完善代码

--- src/app/Http/Controllers/ResetPass.php
+++ src/app/Http/Controllers/ResetPass.php
@@ -22,6 +22,7 @@ class ResetPass extends AbstractController
     protected $viewData;
 //    用户充值密码请求未处理标志
     protected $NOT_DEAL_CODE_NUM;
+    protected $DEAL_CODE_NUM;
     protected $resetViewData;
 
 
@@ -32,6 +33,7 @@ class ResetPass extends AbstractController
 
         $this->viewData = new BindViewData();
         $this->NOT_DEAL_CODE_NUM = 0;
+        $this->DEAL_CODE_NUM = 1;
         $this->resetViewData = [
             'message'=>'',
             'result_check_code'=>''
@@ -108,12 +110,36 @@ class ResetPass extends AbstractController
 
                     $this->viewData->setData($this->resetViewData);
                     $this->viewData->setStatus('success');
+                }else{  //状态为已处理，再次提交
+
+
+                    //更新数据库数据
+                    $resultCheckCode= uniqid($stu_id,true);
+                    $ybResetPass->result_check_code = $resultCheckCode;
+                    $ybResetPass->stu_name = $stu_name;
+                    $ybResetPass->stu_email = $stu_email;
+                    $ybResetPass->old_phone = $old_phone;
+                    $ybResetPass->extra_info = $extra_info;
+                    $ybResetPass->deal_code = $this->NOT_DEAL_CODE_NUM;
+
+
+                    if($ybResetPass->save()){
+                        $this->resetViewData['message'] = '再次重置密码申请提交成功，扫描下方二维码或点击链接查看进度。';
+                        $this->resetViewData['result_check_code'] = $resultCheckCode;
+                        $this->viewData->setData($this->resetViewData);
+                        $this->viewData->setStatus('success');
+                    }else{
+                        $this->resetViewData['message'] = '重置密码申请提交失败，请稍后重试！';
+                        $this->viewData->setData($this->resetViewData);
+                        $this->viewData->setStatus('error');
+                    }
+
                 }
             } else{
 
 
-                $ORMYbResetPass->stu_id = $stu_id;
                 $resultCheckCode= uniqid($stu_id,true);
+                $ORMYbResetPass->stu_id = $stu_id;
                 $ORMYbResetPass->result_check_code = $resultCheckCode;
                 $ORMYbResetPass->stu_name = $stu_name;
                 $ORMYbResetPass->stu_email = $stu_email;
@@ -121,7 +147,7 @@ class ResetPass extends AbstractController
                 $ORMYbResetPass->extra_info = $extra_info;
                 if($ORMYbResetPass->save()){
 
-                    $this->resetViewData['message'] = '重置密码申请提交成功，扫描下方二维码或点击链接查看进度';
+                    $this->resetViewData['message'] = '重置密码申请提交成功，扫描下方二维码或点击链接查看进度。';
                     $this->resetViewData['result_check_code'] = $resultCheckCode;
                     $this->viewData->setData($this->resetViewData);
                     $this->viewData->setStatus('success');
@@ -134,9 +160,7 @@ class ResetPass extends AbstractController
         }
 
 
-
-
-
+//
 //        var_dump($valid->getResult());
 //        var_dump($this->viewData);
 
@@ -147,8 +171,17 @@ class ResetPass extends AbstractController
 
     public function resetPassResult(Request $request, Response $response, array $args){
         $this->viewData->setStatus('success');
-        $this->resetViewData['message'] = '查询码';
         $this->resetViewData['result_check_code'] = $request->getAttribute('route')->getArgument('result_check_code');
+
+        $ORMYbResetPass = YbResetPass::where('result_check_code', '=', $request->getAttribute('route')->getArgument('result_check_code'))->first();
+
+        if($this->NOT_DEAL_CODE_NUM === intval($ORMYbResetPass->deal_code)){
+
+            $this->resetViewData['message'] = '正在处理中，请稍后再查询。';
+        }else if($this->DEAL_CODE_NUM === intval($ORMYbResetPass->deal_code)){
+
+            $this->resetViewData['message'] = $ORMYbResetPass->deal_status_text;
+        }
         $this->viewData->setData($this->resetViewData);
         $this->ci->view->render($response, 'resetPassResult.twig', $this->viewData->toArray());
         return $response;
